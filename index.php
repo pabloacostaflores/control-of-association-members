6cbdb3af59283406f056680f56ac89dad0b2ddfe
Jose Juan Suarez Elizalde <jojusuarez46@gmail.com>
2021-12-12 02:13:03 -0600

    botones en tabla

--- index.php
+++ index.php
@@ -319,7 +319,16 @@
         async defer></script>
     <script> 
         var table = document.getElementById('dataTable');
-        
+        for(var i = 1; i < table.rows.length; i++) {
+            table.rows[i].insertCell(7);
+            for(var j = 0; j < table.rows[i].cells.length; j++) {
+                if(j == 4) {
+                    table.rows[i].cells[j].innerHTML = "<button>Ver</button>";
+                } else if(j == 7) {
+                    table.rows[i].cells[j].innerHTML = "<button style='background:red;'>X</button>";
+                }
+            }            
+        }
     </script>
     <script src="assets/bootstrap/js/bootstrap.min.js"></script>
     <script src="assets/js/theme.js"></script>
