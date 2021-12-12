9b00c99855bdd7de1075f3d696473173c61f52cd
AngelCh415 <angelchavezmorones@gmail.com>
2021-12-12 15:30:04 -0600

    CoordX y CoordY guardado

--- ingresopersonas.php
+++ ingresopersonas.php
@@ -5,13 +5,15 @@ if(isset($_POST['aniadir'])){
     $nombre = $_POST['nombre'];
     $telefono = $_POST['telefono'];
     $contrato = $_POST['IdContrato'];
+    $coordX = $_POST['coorx'];
+    $coordY = $_POST['coory'];
     if(isset($_POST['esSocio'])){
         $socio = 1;
         if(isset($_POST['esHeredero'])){
             $heredero = 1;
     }
 }
-    $sql = "INSERT INTO persona(idPersona, Nombre, Telefono, Dirreccion, EsSocio, EsHeredero) VALUES ('$contrato','$nombre','$telefono','$contrato','$socio','$heredero')";
+    $sql = "INSERT INTO persona(idPersona, Nombre, coordX,coordY,Telefono,  EsSocio, EsHeredero) VALUES ('$contrato','$nombre','$coordX','$coordY','$telefono','$socio','$heredero')";
     $result = mysqli_query($conn, $sql);
     if(!$result){
         echo "<script>alert('Error');</script>";
